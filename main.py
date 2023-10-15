8e22636bc6980cdb2680f88940633c8f2c6da10c
Kenneth <129323640+klavoie1@users.noreply.github.com>
2023-10-15 14:48:53 -0400

    created basic window that displays level

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 import sys
 from settings import *
+from level import Level
 
 pygame.init()
 
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
+level = Level(map_level, window)
 
 while True:
     for event in pygame.event.get():
@@ -14,6 +16,7 @@ while True:
             sys.exit()
 
     window.fill('black')
+    level.run() 
 
     pygame.display.update()
     clock.tick(60)
